3e2caa5cccdf458a1aad5f3caa3227ac06558ed9
Ana Sutreac <asutreac@endava.com>
2018-11-29 13:48:36 +0200

    add Facade pattern

--- Lab_2_IPP/Lab_2_IPP/Facade/Bank.cs
+++ Lab_2_IPP/Lab_2_IPP/Facade/Bank.cs
@@ -4,5 +4,24 @@ using System.Text;
 
 namespace Lab_2_IPP.Facade
 {
-    
+    class Bank {
+
+        private string bankName;
+        private string centralLocation;
+
+        public Bank()
+        {
+
+        }
+
+        public string getBank()
+        {
+            return bankName;
+        }
+
+        public void setBank(string name)
+        {
+            bankName = name;
+        }
+    }
 }
--- Lab_2_IPP/Lab_2_IPP/Facade/Currency.cs
+++ Lab_2_IPP/Lab_2_IPP/Facade/Currency.cs
@@ -6,5 +6,32 @@ namespace Lab_2_IPP.Facade
 {
     class Currency
     {
+        private string currencyType;
+        private double changeCost;
+
+        public Currency(string currencyType, double changeCost)
+        {
+            this.currencyType = currencyType;
+            this.changeCost = changeCost;
+        }
+
+        public void setType(string type)
+        {
+            currencyType = type;
+        }
+
+        public string getType()
+        {
+            return currencyType;
+        }
+
+        public double changeCurrency(double amount)
+        {
+            double rest;
+
+            rest = amount * changeCost;
+
+            return amount; 
+        }
     }
 }
